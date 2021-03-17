6a1dc90eec49086a7a06014c7dc049d3246364ba
Andreza Secon <andrezasecon@gmail.com>
2021-03-16 23:48:58 -0300

    created the classes and performed the relationship of entities

--- src/main/java/com/devsuperior/jpa/entities/Category.java
+++ src/main/java/com/devsuperior/jpa/entities/Category.java
@@ -1,5 +1,57 @@
 package com.devsuperior.jpa.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity // informa ao Spring que esta classe é uma entidate no banco
+@Table(name = "tb_category") // nomeia a tabela
 public class Category {
 
+	@Id // informa ao Spring que esse atribudo é um identificador (*ID)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	private Set<Product> products = new HashSet<>();
+	
+	public Category() {
+
+	}
+
+	public Category(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
+	}
+	
 }
+	
---
+++ src/main/java/com/devsuperior/jpa/entities/Product.java
@@ -0,0 +1,67 @@
+package com.devsuperior.jpa.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Product {
+	
+	private Long id;
+	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
+	
+	private Set<Category> categories = new HashSet<>();
+	
+	public Product() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
+	}
+
+		
+}
+	
+	
