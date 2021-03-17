5908de6e57558634a33a57d6436bf9316fe9557e
Andreza Secon <andrezasecon@gmail.com>
2021-03-17 00:04:42 -0300

    configured h2 on properties file and create tables

--- src/main/java/com/devsuperior/jpa/entities/Product.java
+++ src/main/java/com/devsuperior/jpa/entities/Product.java
@@ -3,15 +3,32 @@ package com.devsuperior.jpa.entities;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity // informa ao Spring que esta classe é uma entidate no banco
+@Table(name = "tb_product") // nomeia a tabela
 public class Product {
 	
+	@Id // informa ao Spring que esse atribudo é um identificador (*ID)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String description;
 	private Double price;
 	private String imgUrl;
 	
-	private Set<Category> categories = new HashSet<>();
+	@ManyToMany // informa ao Spring que o relacionamento será de muitos para muitos
+	@JoinTable(name = "tb_product_category",
+	joinColumns = @JoinColumn(name = "product_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id")) // informando ao Spring o relacionamento
+	private Set<Category> categories = new HashSet<>();     // entre as tabelas pelo ID
 	
 	public Product() {
 
